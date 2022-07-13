e1b70887b20d0082067e6022823ddc5a813391a6
root <root@ip-172-31-34-100.eu-west-1.compute.internal>
2022-07-13 10:19:46 +0000

    Create Vhost
    add certificates ssl and vhost configuration ssl

--- create-vhost.sh
+++ create-vhost.sh
@@ -51,26 +51,99 @@ echo "<VirtualHost *:80>
   ErrorLog /home/${USER}/log/error.log
 </VirtualHost>
 " > /etc/apache2/sites-available/${DOMAIN}.conf
-        echo "Apache - Checking the Apache SSL configuration"
-        /usr/sbin/apache2ctl configtest > /dev/null 2>&1
+    /usr/sbin/a2ensite ${DOMAIN}.conf
+    echo "Apache - Checking the Apache configuration"
+    /usr/sbin/apache2ctl configtest > /dev/null 2>&1
+    RESULT=$?
+    if [ $RESULT -eq 0 ]; then
+        echo "Apache - configtest OK"
+    else
+        echo "Apache - configtest failed"
+        exit 1
+    fi
+    echo "Apache - config reload"
+    /etc/init.d/apache2 reload > /dev/null 2>&1
+
+    certbot certonly --non-interactive --email floriandjerbi@gmail.com --agree-tos --expand --webroot --webroot-path /var/www/html/${USER} --domain ${DOMAIN} --domain www.${DOMAIN}   > /dev/null 2>&1
         RESULT=$?
         if [ $RESULT -eq 0 ]; then
-                 echo "Apache - configtest OK"
+                 _success "Apache - SSL create"
         else
-                echo "Apache - configtest failed"
+                _error "Apache - SSL fail"
                 exit 1
         fi
-        echo "Apache - config reload"
-        /etc/init.d/apache2 reload > /dev/null 2>&1
+
+echo"<VirtualHost *:443>
+  ServerName ${DOMAIN}
+  ServerAlias www.${DOMAIN}
+  DocumentRoot /var/www/html/${USER}
+
+  <IfModule mod_suexec.c>
+    SuexecUserGroup \"${USER}\" \"${USER}\"
+  </IfModule>
+
+
+#  Protocols h2 h2c http/1.1
+
+  # SSL
+  SSLEngine on
+  SSLCertificateFile /etc/letsencrypt/live/${DOMAIN}/cert.pem
+  SSLCertificateKeyFile /etc/letsencrypt/live/${DOMAIN}/privkey.pem
+  SSLCertificateChainFile /etc/letsencrypt/live/${DOMAIN}/chain.pem
+
+  # Apache
+  AddDefaultCharset UTF-8
+  Options +FollowSymLinks -Indexes
+
+  # Deflate
+  AddOutputFilterByType DEFLATE text/html text/css application/javascript application/x-javascript application/xml application/xhtml+xml
+
+  # Etag
+  FileETag None
+
+  <Directory /var/www/html/${USER}>
+   AllowOverride All
+   Require all granted
+
+   Options +FollowSymLinks -Indexes
+  </Directory>
+
+  CustomLog /home/${USER}/log/access.log combined
+  ErrorLog /home/${USER}/log/error.log
+</VirtualHost>
+<VirtualHost */80>
+    ServeurName ${DOMAIN}
+    ServeurAlias www.${DOMAIN}
+
+    RedirectPermanent / https://www.${DOMAIN}/
+" > /etc/apache2/sites-available/${DOMAIN}.conf
+
+    echo "Apache - Checking the Apache SSL configuration"
+    /usr/sbin/apache2ctl configtest > /dev/null 2>&1
+    RESULT=$?
+    if [ $RESULT -eq 0 ]; then
+        echo "Apache - configtest OK"
+    else
+        echo "Apache - configtest failed"
+        exit 1
+    fi
+    echo "Apache - config reload"
+    /etc/init.d/apache2 reload > /dev/null 2>&1
 }
+
 
 function main() {
     echo "User creation"
     usercreation
     echo "User has been created"
 
+    echo "Apache - Vhost creation"
     createvhost
-    echo "Apache - The Vhost was created"
+    echo "Apache - Vhost was created"
+
+    echo "Logrotate creation"
+    createlogrotate
+    echo "Logrotate was created"
 }
 
 
