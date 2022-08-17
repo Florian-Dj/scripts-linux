903171e2e64ce35999a62854cd35812c14d1f3de
FlorianD <floriandjerbi@gmail.com>
2022-08-17 11:23:12 +0000

    Add a verification to sure delete user

--- delete-vhost.sh
+++ delete-vhost.sh
@@ -12,22 +12,41 @@
 #
 
 function main() {
+    init "$@"
+}
+
+function init(){
+    while true; do
+        for user in $(awk -F: '{if (65000 > $3 && $3 > 1000) {print $1}}' /etc/passwd); do
+            echo "  - ${user}"
+        done
+        echo -n "Choice user delete: "
+        read USER
+        getent passwd ${USER} > /dev/null 2>&1
+        RES=$?
+        if [ ${RES} -eq 0 ]; then
+            verification "${USER}"; break;
+        fi
+    done
+}
+
+
+function verification(){
+    while true; do
+        echo -n "Are you sure to delete ${USER}: "
+	read DELETE
+	case ${DELETE} in
+            [Yy]*) delete_user "$USER"; break;;
+            [Nn]*) exit 1;;
+        esac
+    done
+}
+
+function delete_user(){
     BRANCH=$(sudo -Hiu ${USER} bash -c 'echo "${BRANCH}"')
     REPO=$(sudo -Hiu ${USER} bash -c 'echo "${REPO}"')
     DOMAIN=$(sudo -Hiu ${USER} bash -c 'echo "${DOMAIN}"')
     echo "${USER}, ${DOMAIN}, ${REPO}, ${BRANCH}"
 }
 
-while true; do
-    for user in $(awk -F: '{if (65000 > $3 && $3 > 1000) {print $1}}' /etc/passwd); do
-        echo "  - ${user}"
-    done
-    echo -n "Choice user delete: "
-    read USER
-    getent passwd ${USER} > /dev/null 2>&1
-    RES=$?
-    if [ ${RES} -eq 0 ]; then
-        main "${USER}"; break;
-    fi
-done
-
+main "$@"
